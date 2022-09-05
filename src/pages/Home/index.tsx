14cc51f9c89b63131de96e32b165a3fd20a6220c
miqueiasbelfort <kawasousa1554@gmail.com>
2022-09-05 10:57:12 -0300

    add money

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,45 +1,79 @@
 import { useState } from "react";
-import {View, Text, StyleSheet, ScrollView, TouchableOpacity} from "react-native"
+import {View, Text, StyleSheet, ScrollView, TouchableOpacity, TextInput} from "react-native"
 import { Ionicons } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 import {useNavigation} from "@react-navigation/native"
 //componnets
 import AddMoney from "../../components/AddMoney";
 
-// array 
-const extrats = [
-    {
-        id: 1,
-        value: 254.97,
-        active: false
-    },
-    {
-        id: 2,
-        value: 3547.95,
-        active: true
-    },
-    {
-        id: 3,
-        value: 574.97,
-        active: false
-    }
-]
-
 function Home(){
 
-    const [addMoney, setAddMoney] = useState(false)
+    // array
+    const [extrats, setExtrats] = useState([
+        {
+            id: 1,
+            value: 254.97,
+            active: false
+        },
+        {
+            id: 2,
+            value: 3547.95,
+            active: true
+        },
+        {
+            id: 3,
+            value: 574.97,
+            active: false
+        }
+    ])
+    let soma = 0
 
-    const [valueActive, setValueActive] = useState<number>(0)
+    const [addMoney, setAddMoney] = useState(false)
+    const [removeMoney, setRemoveMoney] = useState(false)
+    const [valueActive, setValueActive] = useState<string>('')
+
+    const handleAddMoney = () => {
+        extrats.push({id: extrats.length + 1, value: Number(valueActive), active: true})
+        setAddMoney(!addMoney)
+    }
+    const handleRemoveMoney = () => {
+        extrats.push({id: extrats.length + 1, value: Number(valueActive), active: false})
+        setRemoveMoney(!removeMoney)
+    }
+
+    // entradas 
+    const entradas = extrats.reduce((total, currentIten) => currentIten.value && total + total + currentIten.value, 0)
 
     return (
         <ScrollView>
             <View style={styles.container}>
-                {addMoney && 
-                    <AddMoney 
-                        addmoney={addMoney} 
-                        setAddMoney={setAddMoney} 
-                        setValue={setValueActive}
-                    />
+                {addMoney && //Add Money
+                    <View style={styles.containerAddMoney}>
+                        <Text style={styles.titleAddMoney}>Adicionar Ativos</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder='R$ 1.250,99'
+                            keyboardType='numeric'
+                            onChangeText={setValueActive}
+                        />
+                        <TouchableOpacity style={styles.button} onPress={handleAddMoney}>
+                            <Text style={styles.textButton}>Adicionar</Text>
+                        </TouchableOpacity>
+                  </View>
+                }
+                {removeMoney && //Remove Money
+                    <View style={styles.containerAddMoney}>
+                        <Text style={styles.titleAddMoney}>Adicionra Saida</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder='R$ 1.250,99'
+                            keyboardType='numeric'
+                            onChangeText={setValueActive}
+                        />
+                        <TouchableOpacity style={styles.buttonRed} onPress={handleRemoveMoney}>
+                            <Text style={styles.textButton}>Adicionar</Text>
+                        </TouchableOpacity>
+                  </View>
                 }
                 <View style={styles.header}>
                     <Text style={styles.title}>My Balance</Text>
@@ -51,20 +85,22 @@ function Home(){
                             <Ionicons name="add-circle" size={24} color="#007571" />
                         </TouchableOpacity>
                     </View>
-                    <Text style={styles.priceCard}>R$ 1854,99</Text>
+                    <Text style={styles.priceCard}>R$ {entradas}</Text>
                 </View>
                 <View style={styles.card}>
                     <View style={styles.headerCardContainer}>
                         <Text style={styles.titleCardRed}>SAIDA</Text>
-                        <Ionicons name="add-circle" size={24} color="red" />
+                        <TouchableOpacity onPress={() => setRemoveMoney(!removeMoney)}>
+                            <Ionicons name="add-circle" size={24} color="red" />
+                        </TouchableOpacity>
                     </View>
-                    <Text style={styles.priceCard}>R$ 1854,99</Text>
+                    <Text style={styles.priceCard}>R$</Text>
                 </View>
                 <View style={styles.card}>
                     <View style={styles.headerCardContainer}>
                         <Text style={styles.titleCardBlue}>TOTAL</Text>
                     </View>
-                    <Text style={styles.priceCard}>R$ 1854,99</Text>
+                    <Text style={styles.priceCard}>R$</Text>
                 </View>
 
             </View>
@@ -169,4 +205,62 @@ const styles = StyleSheet.create({
         justifyContent:"space-between",
         paddingHorizontal: 10,
     },
+
+    // add money
+    containerAddMoney: {
+        width: 200,
+        height: 200,
+        backgroundColor: "#fff",
+        borderRadius: 7,
+        borderWidth: 1,
+        borderColor: "#ccc",
+        position: "absolute",
+        zIndex: 999,
+        top: "50%",
+        left: "50%",
+        transform: [
+            {translateY: -50},
+            {translateX: -100}
+        ],
+        padding: 10,
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    titleAddMoney: {
+        fontSize: 19,
+        textAlign: "center",
+        color: "#007571",
+    },
+    input: {
+        marginTop: 20,
+        width: "100%",
+        padding: 10,
+        borderBottomWidth: 2,
+        borderColor: "#007571",
+        backgroundColor: "#ececec",
+        borderTopRightRadius: 7,
+        borderTopLeftRadius: 7,
+    },
+    button: {
+        width: "70%",
+        backgroundColor: "#007571",
+        borderRadius: 7,
+        paddingHorizontal: 15,
+        paddingVertical: 5,
+        marginTop: 20,
+    },
+    buttonRed:{
+        width: "70%",
+        backgroundColor: "red",
+        borderRadius: 7,
+        paddingHorizontal: 15,
+        paddingVertical: 5,
+        marginTop: 20,
+    },
+    textButton: {
+        textAlign: "center",
+        color: "#fff",
+        fontSize: 15,
+        fontWeight: "bold",
+    }
 })
